d7695c7b6e7ee0fcd42bd400bac72789008932eb
iamnazar <mobasser1989@gmail.com>
2017-12-02 22:58:25 +0530

    afsdfafs

--- js/app.js
+++ js/app.js
@@ -2,4 +2,24 @@
 
 $(document).ready(function () {
     new WOW().init();
+
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 50,
+        loop:true,
+        margin:20,
+        animateIn:true,
+        animateOut:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });
